cecdfd842b3364945ebf55bedce8a5d3595d65ba
Dagmara Grabowska <dagmarag0@gmail.com>
2023-02-14 19:40:02 +0100

    done

--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -21,8 +21,9 @@ router.post("/create", async (req, res, next) => {
       title: req.body.title,
       genre: req.body.genre,
       plot: req.body.plot,
-      cast: req.body.cast,
+      cast: req.body.celebrity,
     });
+    console.log(req.body.celebrity);
     res.redirect("/movies");
   } catch (error) {
     next(error);
@@ -51,6 +52,46 @@ router.get("/:id", async (req, res, next) => {
     res.render("movies/movie-details.hbs", {
       movieDetails: response,
     });
+  } catch (error) {
+    next(error);
+  }
+});
+
+// GET => renderizar edición de pelcula
+router.get("/:id/edit", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const response = await Movies.findById(id);
+    res.render("movies/edit-movie.hbs", {
+      editMovie: response,
+    });
+  } catch (error) {
+    next(error);
+  }
+});
+
+// POST => editar
+router.post("/:id/edit", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    await Movies.findOneAndUpdate(id, {
+      title: req.body.title,
+      genre: req.body.genre,
+      plot: req.body.plot,
+      cast: req.body.celebrity,
+    });
+    res.redirect("/movies");
+  } catch (error) {
+    next(error);
+  }
+});
+
+// POST => borrar pelicula
+router.post("/:id/delete", async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    await Movies.findByIdAndRemove(id);
+    res.redirect("/movies");
   } catch (error) {
     next(error);
   }
