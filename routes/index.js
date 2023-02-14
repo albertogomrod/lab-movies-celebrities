f2fdee25ebb7ccafcc5fc85168b1749628826cbe
Alberto Gómez <albertogomrod5@gmail.com>
2023-02-14 15:49:55 +0100

    Iteración 1

--- routes/index.js
+++ routes/index.js
@@ -5,4 +5,12 @@ router.get("/", (req, res, next) => {
   res.render("index");
 });
 
+//RUta Celebrities
+const celebritiesRoutes= require("./celebrities.routes.js")
+router.use("/celebrities", celebritiesRoutes)
+
+//Ruta Movies
+const moviesRoutes= require("./movies.routes.js")
+router.use("/movies", moviesRoutes)
+
 module.exports = router;
